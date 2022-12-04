bbf42266a4345e0e74b2c0ba231e30eee3318e29
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-12-04 15:54:24 +0900

    feat: 공동배달 관련 api setting

---
+++ src/services/apis/deliveryApi.js
@@ -0,0 +1,45 @@
+import Interceptor from "services/apis/AxiosInterceptor";
+
+/**
+ * 공동배달 관련 API
+ */
+
+// 배달 글 전체 조회
+export function getAllDeliveryList() {
+  return Interceptor({
+    url: "delivery/order/",
+    method: "get",
+  });
+}
+
+// 배달 글 등록
+export function registerDeliveryPost() {
+  return Interceptor({
+    url: "delivery/order/",
+    method: "post",
+  });
+}
+
+// 배달 글 조회
+export function getDeliveryPost(id) {
+  return Interceptor({
+    url: `delivery/order/${id}`,
+    method: "get",
+  });
+}
+
+// 배달 글 수정
+export function editDeliveryPost(id) {
+  return Interceptor({
+    url: `delivery/order/${id}`,
+    method: "put",
+  });
+}
+
+// 배달 글 삭제
+export function deleteDeliveryPost(id) {
+  return Interceptor({
+    url: `delivery/order/${id}`,
+    method: "delete",
+  });
+}
